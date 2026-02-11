2fcf9aa13625315fc9fd579c9f5d2d8d121a60c5
davidtagmulmashlim2 <david.tagmulmashlim@gmail.com>
2026-02-11 08:51:22 -0500

    Update stock_app.py

--- stock_app.py
+++ stock_app.py
@@ -6,7 +6,8 @@ import requests
 from streamlit_autorefresh import st_autorefresh
 
 # --- הגדרות טלגרם מעודכנות ---
-TELEGRAM_TOKEN = "openai_api_key = st.secrets["OPENAI_API_KEY"]"
+TELEGRAM_TOKEN = "8247425172:AAHXtpgZN7JDKZAYBFhwLBnlWsPYD_xnlgY"
+openai_api_key = st.secrets["OPENAI_API_KEY"]
 TELEGRAM_CHAT_ID = "779151879" # שים לב: וודא שזה ה-ID שקיבלת מ-userinfobot
 def send_telegram_msg(message):
     try:
@@ -156,3 +157,4 @@ if st.session_state.alerts:
             
 
             st.markdown('</div>', unsafe_allow_html=True)
+
